4c0e57c7e98499b08818b90209882847a57cab68
KaelvihN <araneidasword@gmail.com>
2024-05-27 23:24:53 +0800

    移除元素

--- src/array/removeElement/Re27.java
+++ src/array/removeElement/Re27.java
@@ -7,6 +7,13 @@ package array.removeElement;
  **/
 public class Re27 {
     public int removeElement(int[] nums, int val) {
-
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow++] = nums[fast];
+            }
+            fast++;
+        }
+        return slow;
     }
 }
