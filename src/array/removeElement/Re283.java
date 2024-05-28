15db55645d9d170e6f7252b2064629999bd75093
anran.ma <anran.ma@brilliant-solution.com>
2024-05-28 15:56:52 +0800

    移动零

---
+++ src/array/removeElement/Re283.java
@@ -0,0 +1,33 @@
+package array.removeElement;
+
+import java.util.Arrays;
+
+/**
+ * @author: anran.ma
+ * @created: 2024/5/28
+ * @description: 移动零
+ **/
+public class Re283 {
+
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i++] = temp;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        /**
+         * 4,2,4,0,0,3,0,5,1,0
+         * 0,1,0,3,12
+         * 0
+         * 0,1,0,3,12
+         */
+        new Re283().moveZeroes(new int[]{0,1,0,3,12});
+    }
+}
