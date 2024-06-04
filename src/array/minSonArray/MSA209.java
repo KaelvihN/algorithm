16cd49272d910e83597cbf067072878509078159
KaelviHN <araneidasword@gmail.com>
2024-06-04 21:12:21 +0800

    do 209.长度最小的子数组

--- src/array/minSonArray/MSA209.java
+++ src/array/minSonArray/MSA209.java
@@ -9,20 +9,42 @@ import java.util.Arrays;
  **/
 public class MSA209 {
     public int minSubArrayLen(int target, int[] nums) {
-        int sum = 0, idx = nums.length - 1;
-        Arrays.sort(nums);
-        for (; idx > 0; idx--) {
-            sum += nums[idx];
+        // 初始化
+        int slow = 0, fast = 0, sum = nums[0], res = Integer.MAX_VALUE;
+        while (fast < nums.length - 1) {
+            // 如果sum < target,则快指针向后移动
+            if (sum < target) {
+                sum += nums[++fast];
+            }
+            // 如果 sum >= target,则慢指针向后移动
             if (sum >= target) {
-                break;
+                // 判断该长度是否为最小
+                res = Math.min(res, fast - slow + 1);
+                // 为长度为1可以提前退出
+                if (res == 1) {
+                    return res;
+                }
+                sum -= nums[slow];
+                slow++;
             }
         }
-        return sum >= target ? nums.length - idx : 0;
+        // 结尾判断
+        while (sum >= target) {
+            res = Math.min(res, fast - slow + 1);
+            sum -= nums[slow];
+            slow++;
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+
     }
 
     public static void main(String[] args) {
-        int target = 7;
-        int[] nums = {2, 3, 1, 2, 4, 3};
+//        int target = 7;
+        int target = 11;
+//        int target = 4;
+//        int[] nums = {2, 3, 1, 2, 4, 3};
+//        int[] nums = {1,1,1,1,1,1,1,1};
+        int[] nums = {1, 2, 3, 4, 5};
         int res = new MSA209().minSubArrayLen(target, nums);
         System.out.println("res = " + res);
     }
