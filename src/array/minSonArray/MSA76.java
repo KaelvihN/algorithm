361aaf6076b6a385d82a16d4e4fbb57009391be1
KaelviHN <araneidasword@gmail.com>
2024-06-08 12:43:00 +0800

    do 最小覆盖子串

--- src/array/minSonArray/MSA76.java
+++ src/array/minSonArray/MSA76.java
@@ -12,38 +12,48 @@ import java.util.Set;
  **/
 public class MSA76 {
     public static void main(String[] args) {
-        String s = "ADOBECODEBANC", t = "ABCD";
+        String s = "aa", t = "aa";
         System.out.println(new MSA76().minWindow(s, t));
     }
 
     public String minWindow(String s, String t) {
+        if (t.length()>s.length()){
+            return "";
+        }
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < t.length(); i++) {
             char key = t.charAt(i);
             map.put(key, map.getOrDefault(key, 0) + 1);
         }
-        int count = map.size();
-        String res = "";
+        int count = map.size(),start = 0 ,end = 0,min=Integer.MAX_VALUE;
         for (int slow = 0, fast = 0; fast < s.length(); fast++) {
             char key = s.charAt(fast);
             Integer value = map.get(key);
             if (value == null) {
                 continue;
             }
+            if (value==1){
+                count--;
+            }
             map.put(key, value - 1);
-            count--;
             while (count == 0) {
-                res = fast - slow + 1 < res.length() || res.isEmpty() ? s.substring(slow, fast + 1) : res;
+                if (min > fast - slow+1){
+                    min = fast - slow+1;
+                    start = slow;
+                    end = fast;
+                }
                 Integer sValue = map.get(s.charAt(slow));
                 if (sValue == null) {
+                    slow++;
                     continue;
                 }
                 map.put(s.charAt(slow), sValue + 1);
                 if (sValue == 0) {
                     count++;
                 }
+                slow++;
             }
         }
-        return res;
+        return min!=Integer.MAX_VALUE?s.substring(start,end+1):"";
     }
 }
