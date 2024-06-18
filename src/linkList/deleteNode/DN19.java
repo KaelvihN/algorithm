15b112fed2f9790c283af17a8337026f1a948837
anran.ma <anran.ma@brilliant-solution.com>
2024-06-18 13:44:48 +0800

    undo 删除链表的倒数第 N 个结点

---
+++ src/linkList/deleteNode/DN19.java
@@ -0,0 +1,38 @@
+package linkList.deleteNode;
+
+import linkList.ListNode;
+
+/**
+ * @author: anran.ma
+ * @created: 2024/6/18
+ * @description: 删除链表的倒数第 N 个结点 https://leetcode.cn/problems/remove-nth-node-from-end-of-list/description/
+ **/
+public class DN19 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(4);
+//        head.next.next.next.next = new ListNode(5);
+        ListNode listNode = new DN19().removeNthFromEnd(head, 1);
+        while (listNode != null) {
+            System.out.print(listNode.val + " ");
+            listNode = listNode.next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode idx = head;
+        int size = 1;
+        while (idx.next != null) {
+            size++;
+            idx = idx.next;
+        }
+        idx = head;
+        for (int i = 0; i < size - n - 1; i++) {
+            idx = idx.next;
+        }
+        idx.next = idx.next.next;
+        return head;
+    }
+}
