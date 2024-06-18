3af6bac9ca5bd721d101ed46503b78796464f1e2
anran.ma <anran.ma@brilliant-solution.com>
2024-06-18 16:50:10 +0800

    undo 链表相交

---
+++ src/linkList/crossStartNode/CSN160.java
@@ -0,0 +1,53 @@
+package linkList.crossStartNode;
+
+import linkList.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author: anran.ma
+ * @created: 2024/6/18
+ * @description:链表相交 https://leetcode.cn/problems/intersection-of-two-linked-lists/description/
+ **/
+public class CSN160 {
+    /**
+     * Hash法
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> nodeSet = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null) {
+            nodeSet.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (nodeSet.contains(headB)){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
