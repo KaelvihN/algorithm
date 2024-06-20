65c4679688489306864ccc1ee805ad5c5b808f60
anran.ma <anran.ma@brilliant-solution.com>
2024-06-20 16:22:59 +0800

    do: 环形链表 II

--- src/linkList/circularLinkList/CLL142.java
+++ src/linkList/circularLinkList/CLL142.java
@@ -9,6 +9,25 @@ import linkList.ListNode;
  **/
 public class CLL142 {
     public ListNode detectCycle(ListNode head) {
-
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                slow = head;
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 }
